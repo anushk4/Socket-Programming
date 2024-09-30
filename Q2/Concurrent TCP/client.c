370afd3e8f16260f668089d3fbb8973bde6f0222
Apaar Garg <133491385+apaar0001@users.noreply.github.com>
2024-09-30 13:50:24 +0530

    Update client.c

--- Q2/Concurrent TCP/client.c
+++ Q2/Concurrent TCP/client.c
@@ -1,6 +1,7 @@
 #include "handleClient.c"
 
 int main(int argc, char *argv[]) {
+    // Check to ensure the necessary 3 arguments are passed through command line
     if (argc != 3) {
         printf("Usage: %s <server_ip> <num_clients>\n", argv[0]);
         return -1;
@@ -9,23 +10,25 @@ int main(int argc, char *argv[]) {
     char *server_ip = argv[1];
     int num_clients = atoi(argv[2]);
 
+    // Checking the number of client should be greater than 0
     if (num_clients <= 0) {
         printf("Number of clients must be greater than 0\n");
         return -1;
     }
 
+    // To make multiple threads, each for a client
     pthread_t client_threads[num_clients];
     struct client_args clients[num_clients];
 
-    // Create multiple clients and connect to the server
+    // Initiate multiple clients to connect to server
     for (int i = 0; i < num_clients; i++) {
         clients[i].server_ip = server_ip;
         clients[i].client_id = i + 1;
 
-        pthread_create(&client_threads[i], NULL, connect_to_server, &clients[i]);
+        pthread_create(&client_threads[i], NULL, connect_to_server, &clients[i]);  // To ensure multithreading
     }
 
-    // Wait for all clients to finish
+    // Waiting for all clients to finish the execution and then return the result using join
     for (int i = 0; i < num_clients; i++) {
         pthread_join(client_threads[i], NULL);
     }
