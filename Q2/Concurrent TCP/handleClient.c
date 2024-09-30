91d1e17c8daa47896a0f93ba3822cb0016c1a379
Apaar Garg <133491385+apaar0001@users.noreply.github.com>
2024-09-30 13:52:44 +0530

    Update handleClient.c

--- Q2/Concurrent TCP/handleClient.c
+++ Q2/Concurrent TCP/handleClient.c
@@ -1,13 +1,14 @@
 #include "handleClient.h"
 
 void *handle_client_for_connection(void *arg) {
+    // Code for last 3 parts that is getting  the top 2 CPU process
     int client_sock = *(int *)arg;
-    free(arg); // Free dynamically allocated memory for client socket
+    free(arg); 
 
     char buffer[BUFFER_SIZE] = {0};
     struct process_info top_processes[2];
 
-    // Get the client's address
+    // Retrieve the client addresss to let the server know which client is sending the connection request
     struct sockaddr_in client_addr;
     socklen_t addr_len = sizeof(client_addr);
     getpeername(client_sock, (struct sockaddr *)&client_addr, &addr_len);
@@ -19,12 +20,12 @@ void *handle_client_for_connection(void *arg) {
 
     printf("Connected to client at %s:%d\n", client_ip, client_port);
 
-    // Read client request (could be any message, we assume a simple message here)
+    // Read and check the client requesst, here we have hardcoded the message to check that server has recieved a request
     int bytes_read = read(client_sock, buffer, BUFFER_SIZE);
     if (bytes_read > 0) {
         printf("Received request from client: %s\n", buffer);
 
-        // Get the top 2 CPU-consuming processes
+        // Calling the function to retrieve the 2 top 2 processes running on server side
         get_top_cpu_processes(top_processes);
 
         // Log the top 2 processes to the server console
@@ -34,7 +35,7 @@ void *handle_client_for_connection(void *arg) {
         printf("2. PID: %d, Name: %s, User Time: %lu, Kernel Time: %lu\n",
                top_processes[1].pid, top_processes[1].name, top_processes[1].user_time, top_processes[1].kernel_time);
 
-        // Prepare the response with the top 2 processes' information
+        // Print the process info on server side
         snprintf(buffer, sizeof(buffer),
                  "Top 2 CPU-consuming processes:\n"
                  "1. PID: %d, Name: %s, User Time: %lu, Kernel Time: %lu\n"
@@ -46,7 +47,7 @@ void *handle_client_for_connection(void *arg) {
         send(client_sock, buffer, strlen(buffer), 0);
     }
 
-    // Close the client socket
+    // Close the client socket after all the requests are taken care of
     close(client_sock);
     return NULL;
 }
