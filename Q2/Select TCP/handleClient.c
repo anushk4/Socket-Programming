fa7ea6ff106d6665e0e14e72d318388c0aa817fc
Apaar Garg <133491385+apaar0001@users.noreply.github.com>
2024-09-30 14:04:05 +0530

    Update handleClient.c

--- Q2/Select TCP/handleClient.c
+++ Q2/Select TCP/handleClient.c
@@ -2,11 +2,11 @@
 
 void *handle_client_for_connection(int *client_sockets, int i){
     int client_sock = client_sockets[i];
-    // Convert the client's IP address to human-readable form
+    
     char buffer[BUFFER_SIZE] = {0};
     struct process_info top_processes[2];
 
-    // Get the client's address
+     // Retrieve the client addresss to let the server know which client is sending the connection request
     struct sockaddr_in client_addr;
     socklen_t addr_len = sizeof(client_addr);
     getpeername(client_sock, (struct sockaddr *)&client_addr, &addr_len);
@@ -18,21 +18,25 @@ void *handle_client_for_connection(int *client_sockets, int i){
 
     printf("Connected to client at %s:%d\n", client_ip, client_port);
 
-    // Read client request (could be any message, we assume a simple message here)
+    // Read and check the client requesst, here we have hardcoded the message to check that server has recieved a request
     int bytes_read = read(client_sock, buffer, BUFFER_SIZE);
 
     if (bytes_read > 0) {
         // Handle the client's request and send the response
         buffer[bytes_read] = '\0';
         struct process_info top_processes[2];
+
+        // Calling the function to retrieve the 2 top 2 processes running on server side
         get_top_cpu_processes(top_processes);
 
+        // Print the process info on server side
         printf("Top 2 CPU-consuming processes sent to client %s:%d:\n", client_ip, client_port);
         printf("1. PID: %d, Name: %s, User Time: %lu, Kernel Time: %lu\n",
             top_processes[0].pid, top_processes[0].name, top_processes[0].user_time, top_processes[0].kernel_time);
         printf("2. PID: %d, Name: %s, User Time: %lu, Kernel Time: %lu\n",
             top_processes[1].pid, top_processes[1].name, top_processes[1].user_time, top_processes[1].kernel_time);
 
+        // Make the response to be send to the client that is the process info
         snprintf(buffer, sizeof(buffer),
                     "Top 2 CPU-consuming processes:\n"
                     "1. PID: %d, Name: %s, User Time: %lu, Kernel Time: %lu\n"
@@ -42,7 +46,7 @@ void *handle_client_for_connection(int *client_sockets, int i){
 
         send(client_sock, buffer, strlen(buffer), 0);
     }
-    // Client has disconnected
+    // Close the client socket after all the requests are taken care of
     printf("Client disconnected: socket FD is %d\n", client_sock);
     close(client_sock);
     client_sockets[i] = 0; // Remove from client sockets list
